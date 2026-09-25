b3880085b3ac09e750a7a6b50cfcefb762e3f59c
ayush-bug <ayushkushwaharxl2007@gmail.com>
2026-09-25 12:13:22 +0530

    added some spooky conversation and sounds

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const horrorScream = new Audio('/assets/sounds/horror-scream.mp3');
 
 const screamHalloween = new Audio('/assets/sounds/Scream-Halloween-Horror.mp3');
 const nunVoice = new Audio('/assets/sounds/nun-voice.mp3');
-const iseeyou = new Audio('/assets/sounds/iseeyou.mp3')
-
+const iseeyou = new Audio('/assets/sounds/iseeyou.mp3');
+const scarylaugh = new Audio('/assets/sounds/ScaryLaughSoundEffect.mp3');
+const evilscaryLaugh = new Audio('/assets/sounds/Scary-Sound-Effect3.mp3');
+ 
   document.querySelector('body').addEventListener('click', function(){
     horrorScream.play();
   });
@@ -31,9 +33,33 @@ document.getElementById("no").addEventListener("click" , function(){
 
 document.getElementById("yes").addEventListener("click" , function(){
     screamHalloween.play();
-    document.querySelector('.yesNote').style.display = "flex";
-    
+
+        document.querySelector('#txtyes').style.display = "block";
+    document.querySelector('.yesNote').style.display = "flex"; 
+        document.querySelector('#txt2').style.display = "none";
+   
 });
 document.getElementById("btn2").addEventListener('click', function(){
     nunVoice.play();
+});
+
+document.getElementById("btn3").addEventListener('click' , function(){
+    scarylaugh.play();
+});
+
+document.getElementById("btn2").addEventListener('click', function(){
+        document.querySelector('#txtNo').style.display = "none";
+
+    document.querySelector("#really").style.display = "block";
+
+});
+
+document.getElementById("btn3").addEventListener('click' , function(){
+    document.querySelector('.yesNote').style.display = "none";
+    document.querySelector('.hmmOk').style.display = "flex";
+    evilscaryLaugh.play();
+             document.querySelector('#txtyes').style.display = "none";
+
+    document.querySelector('#txt3').style.display = "block";
+
 })
